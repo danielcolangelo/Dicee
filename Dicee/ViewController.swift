02c2cb24d2d12ba97ba2156ddfef6c8846645f9e
danielcolangelo <danalfons@yahoo.com>
2018-06-26 19:30:17 -0400

    Added all the functionality to the app as well as the ability to shake the app to re-roll the dice.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,13 +13,16 @@ class ViewController: UIViewController {
     var randomDiceIndex1: Int = 0
     var randomDiceindex2: Int = 0
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,10 +31,20 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceindex2 = Int(arc4random_uniform(6))
         
-        print(randomDiceIndex1)
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceindex2])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
     }
 }
 
